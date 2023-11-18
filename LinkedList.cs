ab35086fe345fd81e68576510c3d2f2d01ea39f1
Momenize's Device <typinprojects@gmail.com>
2023-11-18 04:59:47 +0330

    Adding files to the repo

---
+++ LinkedList.cs
@@ -0,0 +1,315 @@
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DS_LinkedList{
+    public class LinkedList{
+        public Node _tail;
+        public Node _head;
+
+        public LinkedList()
+        {
+            _tail = null;
+            _head = null;
+        }
+
+        public LinkedList(string data)
+        {
+            AddAtHead(data);
+        }
+
+
+
+
+        public void PrintList()
+        {
+            if(IsEmpty())
+            {
+                throw new Exception("Your list is empty. Try again!");
+            }
+
+            Node n = _head;
+            while(n != null)
+            {
+                Console.Write(n._data + " -> ");
+                n = n.next;
+            }
+            Console.WriteLine("NULL");
+        }
+        
+        
+
+        public void AddAtHead(string data)
+        {
+            Node node = new Node(data);
+            if (IsEmpty())
+            {
+                _tail = node;
+                _head = node;
+                return;
+            }
+
+            node.next = _head;
+            _head.last = node;
+            _head = node;
+
+        }
+
+        private bool IsEmpty()
+        {
+            if(_head == null) return true;
+            return false;
+        }
+
+        public int GetIndexFromHead(string data)
+        {
+            if(Search(data) != null)
+            {
+                int c = 0; 
+                Node node = _head;
+                while(node != null)
+                {
+                    if (node._data.Equals(data))
+                    {
+                        return c;
+                    }
+                    c++;
+                    node = node.next;
+                }
+                return c;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static bool MirrorCheck(LinkedList l)
+        {
+            if (l.IsEmpty()) throw new Exception("Your list is empty. Try again!");
+            
+            Node head = l._head;
+            Node tail = l._tail;
+            while(head != null && tail != null)
+            {
+                if(head._data != tail._data)
+                {
+                    return false;
+                }
+
+                if (head == tail || head == tail.last) return true;
+                
+
+                head = head.next;
+                tail = tail.last;
+            }
+
+            return true;
+
+            
+        }
+        
+        public static void SumEqualsToX(LinkedList l, int x)
+        {
+            if (l.IsEmpty())
+            {
+                throw new Exception("Your list is empty. Try again!");
+            }
+            
+            Node head = l._head;
+            if(head.next == null)
+            {
+                throw new Exception("Your list doesn't contain enough data. Try again!");
+            }
+
+            bool isFirstOne = true;
+            
+            while(head != null)
+            {
+                Node tail = l._tail;
+                if (head == tail) return;
+                
+                while (tail != head)
+                {
+                    if (Convert.ToInt32(head._data) + Convert.ToInt32(tail._data) == x)
+                    {
+                        if (isFirstOne) 
+                        { 
+                            Console.Write("(" + head._data + $", {tail._data})");
+                            isFirstOne = false;
+                        }
+                        else Console.Write(", (" + head._data + $", {tail._data})");
+                    }
+                    tail = tail.last;
+                }
+                head = head.next;
+            }
+        }
+        public int GetIndexFromTail(char data)
+        {
+            if(IsEmpty())
+            {
+                throw new Exception("Your list is empty. Try again!");
+            }
+
+            int c = 0;
+            Node node = _tail;
+            while(node != null)
+            {
+                c++;
+                node = node.last;
+            }
+            return c;
+        }
+        public int Counter()
+        {
+            int c = 0;
+            if (!IsEmpty())
+            {
+                Node n = _head;
+                Node n1 = _tail;
+
+                while(n != null && n1 != null)
+                {
+                    if(n == n1)
+                    {
+                        c++;
+                        break;
+                    }
+                    c += 2;
+                    n = n.next;
+                    n1 = n1.last;
+                }
+                
+            }
+            return c;
+        }
+        public void AddAtTail(string data)
+        {
+            Node node = new Node(data);
+            if (IsEmpty())
+            {
+                _tail = node;
+                _head = node;
+                return;
+            }
+
+            _tail.next = node;
+            node.last = _tail;
+            _tail = node;
+        }
+
+
+        public static void ReversedList(LinkedList l)
+        {
+            var l1 = new LinkedList();
+            Node n = l._tail;
+            while(n != null)
+            {
+                l1.AddAtHead(n._data);
+                n = n.next;
+            }
+
+            l1.PrintList();
+        }
+
+        public static void FixLoop(LinkedList l)
+        {
+            Stack<Node> nextLoop = new Stack<Node>();
+            Stack<Node> lastLoop = new Stack<Node>();
+            Node head = l._head;
+            Node tail = l._tail; 
+            while(head != null)
+            {
+                
+                nextLoop.Push(head);
+                if(head.next == null)
+                {
+                    break;
+                }
+                if (nextLoop.Contains(head.next))
+                {
+                    if(head == tail)
+                    {
+                        head.next = null;
+                        break;
+                    }
+
+                    Node n1 = l._tail;
+                    while(n1 != null)
+                    {
+                        if(n1.last == head)
+                        {
+                            head.next = n1;
+                            break;
+                        }
+                        n1 = n1.next;
+                    }
+                }
+                head = head.next;
+            }
+
+
+            while(tail != null)
+            {
+                lastLoop.Push(tail);
+                if(tail.last == null)
+                {
+                    return;
+                }
+                else if (lastLoop.Contains(tail.last))
+                {
+                    
+                    head = l._head;
+                    if (tail == head)
+                    {
+                        tail.last = null;
+                        return;
+                    }
+                    while(head != null)
+                    {
+                        if(head.next == tail)
+                        {
+                            tail.last = head;
+                            break;
+                        }
+                        head = head.next;
+                    }
+                }
+                tail = tail.last;
+            }
+        }
+
+        
+
+        public Node Search(string data)
+        {
+            Node node = _tail;
+            Node node1 = _head;
+
+            while(node != null && node1 != null)
+            {
+                if(node == node1)
+                {
+                    if (node._data.Equals(data)) return node;
+                    return null;
+                }
+                
+                if(node._data.Equals(data))
+                {
+                    return node;
+                }
+                if (node1._data.Equals(data)) return node1;
+                node = node.next;
+                node1 = node1.last;
+            }
+            return null;
+        }
+        
+        
+
+        
+
+    }
+
+}
---
+++ Node.cs
@@ -0,0 +1,13 @@
+namespace DS_LinkedList{
+    public class Node{
+
+        internal string _data;
+        internal Node next;
+        internal Node last;
+
+        public Node(string data)
+        {
+            _data = data; 
+        }
+    }
+}
