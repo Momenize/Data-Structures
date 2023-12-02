827624dc0c5e8742057109471cccd2c937ba0691
Momenize's Device <typinprojects@gmail.com>
2023-12-02 22:38:36 +0330

    Updated this branch

---
+++ LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DS_LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] data =
+            {
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" }
+            };
+
+            
+            LinkedList.PrintTwoDimList(LinkedList.TwoDimlist(data));
+        }
+    }
+}
